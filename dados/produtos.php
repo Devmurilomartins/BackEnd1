96daa4e958446c1d1c0d443448c9701f63f80a70
devmurilomartins <murilo.nmartins@gmail.com>
2022-08-10 23:30:46 -0300

    first commit

---
+++ dados/produtos.php
@@ -0,0 +1,7 @@
+<?php
+$produto = [
+    ['id' => 1, 'nome' => 'Marmitao peso pesado', 'valor' => 38.00, 'tipo' => 'Pesado', 'peso' => '1 kg', 'descricao' => 'Feito Arroz, feijão, frango grelhado, purê de batata e saladas', 'imagem' => '  images/nakedcakebombom.jpg'],
+    ['id' => 2, 'nome' => 'Marmita Basica', 'valor' => 32.00, 'tipo' => 'Leve', 'peso' => '1,2 kg', 'descricao' => 'Arroz, feijão e picadinho de carne com legumes! !', 'imagem' => 'images/nakedcakebombom.jpg'],
+    ['id' => 4, 'nome' => 'Marmita Fit', 'valor' => 43.00, 'tipo' => 'Fit', 'peso' => '1 kg', 'descricao' => 'Macarrão de batata-doce com legumes e cogumelos.', 'imagem' => 'images/nakedcakebombom.jpg'],
+    ['id' => 5, 'nome' => 'Marmita Nanica', 'valor' => 25.00, 'tipo' => 'Infantil', 'peso' => '1,1 kg', 'descricao' => 'Nhoque de batata e cenoura, arroz e lentilha.', 'imagem' => 'images/nakedcakebombom.jpg']
+];
---
+++ pages/footer.php
@@ -0,0 +1,33 @@
+<footer>
+<div class="wrapper">
+  <div class="footer-box">
+    <div class="company-footer">       
+       <h2>RA:21100508-5</h2>
+       <h2>Murilo Nunes Martins</h2>
+       <h2>PROGRAMAÇÃO BACK END I</h2>
+    </div>       
+   </div>
+   <div class="footer-box">     
+     <ul class="footer-list">
+        <li>
+           <h1>Nossas redes sociais</h1>
+        </li>
+      
+        <li> 
+         <a href="#" target="_blank">
+           <i class="fa fa-instagram" style="font-size:24px"></i>
+           Instagram        
+         </a>
+        </li> 
+       
+     </ul>
+   </div>
+ </div>
+ <div class="footer-bottom">  
+  <div>
+    <h2 class="direitos">21100508-5 Murilo Nunes Martins</h2>
+    </div>
+  </div>
+</footer>
+  
+ 
